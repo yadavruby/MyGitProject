be5e5489148b3f205b3be2144a9504334b371c27
Ruby <Ruby @DESKTOP-V5H6KLI.attlocal.net>
2018-02-13 17:33:30 -0600

    Test commit

---
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	//testng xml file from Maven
+	//how to exceute all test cases from Test folder - mvn test
+	@Test
+	public void BrowserAutomation()
+	
+	{
+		System.out.println("BrowserAutomation");
+
+	}
+	
+	@Test 
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+
+	}
+}
+
